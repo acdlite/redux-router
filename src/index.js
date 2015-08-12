0a037e058267391d958aad9c87b53fb212de26f8
Igor Scekic <igorscekic2@gmail.com>
2015-08-12 16:41:20 +0200

    add replaceWith to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import routerStateReducer from './routerStateReducer';
 import reduxRouteComponent from './reduxRouteComponent';
 import transitionTo from './transitionTo';
+import replaceWith from './replaceWith';
 
 export {
   routerStateReducer,
   reduxRouteComponent,
-  transitionTo
+  transitionTo,
+  replaceWith
 };
