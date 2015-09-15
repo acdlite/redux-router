d94cedf67c828940d7688ba6d97d1effa368f6e4
Andrew Clark <acdlite@me.com>
2015-09-15 11:41:28 -0700

    Fix gitignore to only ignore root server.js

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import { compose, applyMiddleware } from 'redux';
+import createMemoryHistory from 'history/lib/createMemoryHistory';
+import baseReduxReactRouter from './reduxReactRouter';
+import transformOptions from './transformOptions';
+import matchMiddleware from './matchMiddleware';
+import { MATCH } from './constants';
+
+function server(next) {
+  return options => createStore => (reducer, initialState) => {
+    const store = compose(
+      applyMiddleware(
+        matchMiddleware((...args) => store.history.match(...args))
+      ),
+      next({
+        ...options,
+        createHistory: options.createHistory || createMemoryHistory
+      })
+    )(createStore)(reducer, initialState);
+    return store;
+  };
+}
+
+export function match(url, callback) {
+  return {
+    type: MATCH,
+    payload: {
+      url,
+      callback
+    }
+  };
+}
+
+export const reduxReactRouter = compose(
+  transformOptions,
+  server
+)(baseReduxReactRouter);
