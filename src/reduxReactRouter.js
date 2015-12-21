0cdf254cfa8b71801f70ff5d597164003e5bdb06
Matthew Russell <matt@simspace.com>
2015-12-21 15:01:00 -0800

    Check if function type as well

--- src/reduxReactRouter.js
+++ src/reduxReactRouter.js
@@ -28,7 +28,8 @@ export default function reduxReactRouter({
     [ 'pushState', 'push', 'replaceState', 'replace',
       'setState', 'go', 'goBack', 'goForward',
       'listen', 'createLocation', 'match' ].forEach(funcName => {
-        if (!history.hasOwnProperty(funcName)) {
+        if (!history.hasOwnProperty(funcName) &&
+            typeof(history[funcName]) === 'function') {
           throw new Error(`History API does not support function: ${funcName}`);
         }
       });
