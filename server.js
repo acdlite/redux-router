7fbf6529329a1f96f18f68d018c364a6172babc2
Remy Loubradou <remy.loubradou@sigfox.com>
2017-10-12 13:40:31 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// This file is copied to the root of the project to allow
+'use strict';
+
+exports.__esModule = true;
+
+var _libServer = require('./lib/server');
+
+exports.reduxReactRouter = _libServer.reduxReactRouter;
+exports.match = _libServer.match;
