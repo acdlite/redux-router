f19b8fcca00701bdf7c34a9ce58b042d03dca872
Laetitia Dufau <laetitia.dufau@sigfox.com>
2021-07-07 14:43:00 +0200

    chore: bump react-redux to v7

--- src/ReduxRouter.js
+++ src/ReduxRouter.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { RouterContext as DefaultRoutingContext } from '@sigfox/react-router';
-import { createRouterObject } from '@sigfox/react-router/lib/RouterUtils';
-import routerStateEquals from './routerStateEquals';
-import { ROUTER_STATE_SELECTOR } from './constants';
-import { initRoutes, replaceRoutes } from './actionCreators';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect, useStore } from "react-redux";
+import { RouterContext as DefaultRoutingContext } from "@sigfox/react-router";
+import { createRouterObject } from "@sigfox/react-router/lib/RouterUtils";
+import routerStateEquals from "./routerStateEquals";
+import { ROUTER_STATE_SELECTOR } from "./constants";
+import { initRoutes, replaceRoutes } from "./actionCreators";
 
 function memoizeRouterStateSelector(selector) {
   let previousRouterState = null;
 
-  return state => {
+  return (state) => {
     const nextRouterState = selector(state);
     if (routerStateEquals(previousRouterState, nextRouterState)) {
       return previousRouterState;
@@ -24,22 +24,28 @@ function getRoutesFromProps(props) {
   return props.routes || props.children;
 }
 
+function withStore(ClassComponent) {
+  return function WrappedComponent(props) {
+    const store = useStore();
+    return <ClassComponent {...props} store={store} />;
+  };
+}
+
 class ReduxRouter extends Component {
   static propTypes = {
-    children: PropTypes.node
-  }
-
-  static contextTypes = {
-    store: PropTypes.object
-  }
+    children: PropTypes.node,
+  };
 
   constructor(props, context) {
     super(props, context);
-    this.router = createRouterObject(context.store.history, context.store.transitionManager);
+    this.router = createRouterObject(
+      this.props.store.history,
+      this.props.store.transitionManager
+    );
   }
-  
+
   UNSAFE_componentWillMount() {
-    this.context.store.dispatch(initRoutes(getRoutesFromProps(this.props)));
+    this.props.store.dispatch(initRoutes(getRoutesFromProps(this.props)));
   }
 
   UNSAFE_componentWillReceiveProps(nextProps) {
@@ -49,30 +55,25 @@ class ReduxRouter extends Component {
   receiveRoutes(routes) {
     if (!routes) return;
 
-    const { store } = this.context;
-    store.dispatch(replaceRoutes(routes));
+    this.props.store.dispatch(replaceRoutes(routes));
   }
 
   render() {
-    const { store } = this.context;
-
-    if (!store) {
+    if (!this.props.store) {
       throw new Error(
-        'Redux store missing from context of <ReduxRouter>. Make sure you\'re '
-      + 'using a <Provider>'
+        "Redux store missing from context of <ReduxRouter>. Make sure you're " +
+          "using a <Provider>"
       );
     }
 
-    const {
-      history,
-      [ROUTER_STATE_SELECTOR]: routerStateSelector
-    } = store;
+    const { history, [ROUTER_STATE_SELECTOR]: routerStateSelector } =
+      this.props.store;
 
     if (!history || !routerStateSelector) {
       throw new Error(
-        'Redux store not configured properly for <ReduxRouter>. Make sure '
-      + 'you\'re using the reduxReactRouter() store enhancer.'
-    );
+        "Redux store not configured properly for <ReduxRouter>. Make sure " +
+          "you're using the reduxReactRouter() store enhancer."
+      );
     }
 
     return (
@@ -86,17 +87,15 @@ class ReduxRouter extends Component {
   }
 }
 
-@connect(
-  (state, { routerStateSelector }) => routerStateSelector(state) || {}
-)
+@connect((state, { routerStateSelector }) => routerStateSelector(state) || {})
 class ReduxRouterContext extends Component {
   static propTypes = {
     location: PropTypes.object,
-    RoutingContext: PropTypes.func
-  }
+    RoutingContext: PropTypes.func,
+  };
 
   render() {
-    const {location} = this.props;
+    const { location } = this.props;
 
     if (location === null || location === undefined) {
       return null; // Async matching
@@ -108,4 +107,4 @@ class ReduxRouterContext extends Component {
   }
 }
 
-export default ReduxRouter;
+export default withStore(ReduxRouter);
